4ced1a0f469c87931b0c1b7385d0cd42f96f2180
nimetozk <nimet.ozakca@city.ac.uk>
2021-02-21 23:21:05 +0000

    add new services for user

--- src/services/service.js
+++ src/services/service.js
@@ -24,6 +24,54 @@ class Service {
       headers: { Authorization: localStorage.getItem("token") },
     });
   }
+
+  getBankList() {
+    return httpClient.get("/api/bank/list", {
+      headers: { Authorization: localStorage.getItem("token") },
+    });
+  }
+
+  insertBank(bankModel) {
+    return httpClient.post("/api/bank", bankModel, {
+      headers: { Authorization: localStorage.getItem("token") },
+    });
+  }
+
+  getBankById(id) {
+    return httpClient.get(`/api/bank/${id}`, {
+      headers: { Authorization: localStorage.getItem("token") },
+    });
+  }
+
+  insertBankAccount(bankAccountModel) {
+    return httpClient.post("/api/bankaccount", bankAccountModel, {
+      headers: { Authorization: localStorage.getItem("token") },
+    });
+  }
+
+  getDeleteBankAccountById(id) {
+    return httpClient.delete(`/api/bankaccount/${id}`, {
+      headers: { Authorization: localStorage.getItem("token") },
+    });
+  }
+
+  getCurrentUser() {
+    return httpClient.get("/api/user/current", {
+      headers: { Authorization: localStorage.getItem("token") },
+    });
+  }
+
+  updateUser(user) {
+    return httpClient.put("/api/user", user, {
+      headers: { Authorization: localStorage.getItem("token") },
+    });
+  }
+
+  getCurrentUserBankAccounts() {
+    return httpClient.get("/api/bankaccount/useraccounts", {
+      headers: { Authorization: localStorage.getItem("token") },
+    });
+  }
 }
 
 export default new Service();
